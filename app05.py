c631d717cbc6fcb9714a8e1ed46ad10192399fab
Nikhil09052007 <pandeynikhil0905@gmail.com>
2026-04-03 13:16:39 +0530

    appo5 for uploading files

---
+++ app05.py
@@ -0,0 +1,9 @@
+# Working with uploading the data.
+
+import streamlit as st 
+
+uploaded_file = st.file_uploader("Upload a text file: ",type = ["text"])
+
+if uploaded_file is not None:
+    content = uploaded_file.read().decode('utf-8')
+    st.text_area("File content",content,height=200)
