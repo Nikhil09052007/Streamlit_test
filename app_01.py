de0c6b315885508840f5702328bbad2f8da4e382
Nikhil09052007 <pandeynikhil0905@gmail.com>
2026-04-03 11:48:27 +0530

    Commited

--- app_01.py
+++ app_01.py
@@ -1,5 +1,8 @@
 import streamlit as st 
 
+# Basic syntax used in streamlit to perform the representation of the frontened.
+
+
 st.title("This is the title.")
 st.header("This is header")
 st.subheader("This is subheader")
@@ -20,3 +23,16 @@ print(New("Streamlit")
 ''', language="python")
 
 st.markdown("### Inline Latex: $a^2 + b^2 = c^2$ ")
+
+
+st.success("This is a success message.")
+
+st.warning("Be careful! This is a warning message.")
+
+st.error("Something went wrong!, This rerpresents error..")
+
+st.info("This is an info messagese.")
+
+st.markdown("> **Tip:** Use feedback to guide the user.")
+
+
